5d24a2d79c49e3d7424a2e72959cfc794e39bf65
vinayreddy941 <vinayeee2001@gmail.com>
2024-02-08 16:25:11 +0530

    updated

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+const express = require('express')
+const app = express();
+const tasksRouter =require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//midleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//routes 
+
+
+
+
+
+app.use('/api/v1/tasks', tasksRouter)
+
+const port = 3000;
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}.. `))
+    }   catch (error){
+        console.log(error)
+
+    }
+}
+
+start()
+
+
